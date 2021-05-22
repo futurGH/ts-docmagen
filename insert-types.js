033aefe291db7ade48c5336374f9d109266b5fdf
Futuristick <728608@pdsb.net>
2021-05-22 15:45:51 -0400

    Small refactor + support for more class member documentation
    Now automatically generates documentation for:
    - class property initializer (default) values
    - class member modifiers (public, private, protected, readonly, static)

--- insert-types.js
+++ insert-types.js
@@ -1,30 +1,46 @@
 const path = require("path");
-const { Project, ScriptTarget, ts } = require("ts-morph");
+const { Project, ScriptTarget, ts, Node } = require("ts-morph");
+
+// still not sure what the difference between these three is
+Node.isClassProperty = (node) => {
+    return Node.isPropertyDeclaration(node) || Node.isPropertyAssignment(node) || Node.isPropertySignature(node)
+}
+
+function getJsDocOrCreate(node) {
+    return node.getJsDocs()[0] || node.addJsDoc({});
+}
 
 function generateParameterDocumentation(functionNode) {
     const params = functionNode.getParameters()
     for (const param of params) {
-        const paramTypeNode = param.getTypeNode()
-        if (!paramTypeNode) continue;
-        for (const paramTag of (functionNode?.getJsDocs()?.[0]?.getTags() || []).filter(tag =>
-          ["param", "parameter"].includes(tag.getTagName())
-        )) {
+        const parameterType = param.getTypeNode()?.getText()
+        if (!parameterType) continue;
+        // Get param tag that matches the param
+        const jsDoc = getJsDocOrCreate(functionNode);
+        const paramTag = (jsDoc.getTags() || [])
+          .filter(tag => ["param", "parameter"].includes(tag.getTagName()))
+          .find(tag => tag.compilerNode.name?.getText() === param.getName())
+        
+        const paramName = param.compilerNode.name?.getText()
+        if (paramTag) {
             // Replace tag with one that contains typing info
-            const name = paramTag.compilerNode.name.getText()
-            if (name !== functionNode.getName()) continue;
             const comment = paramTag.getComment()
-            const parameterType = paramTypeNode.getText()
             const tagName = paramTag.getTagName()
-            
-            paramTag.replaceWithText(`@${tagName} {${parameterType}} ${name}  ${comment}`)
+
+            paramTag.replaceWithText(`@${tagName} {${parameterType}} ${paramName}  ${comment}`)
+        } else {
+            jsDoc.addTag({
+                tagName: "param",
+                text: `{${parameterType}} ${paramName}`
+            })
         }
     }
 }
 
 function generateReturnTypeDocumentation(functionNode) {
     const functionReturnType = functionNode.getReturnType()?.getText()
-    const jsDoc = functionNode.getJsDocs()?.[0]
-    const returnsTag = (jsDoc.getTags() || []).find(tag =>
+    const jsDoc = getJsDocOrCreate(functionNode)
+    const returnsTag = (jsDoc?.getTags() || []).find(tag =>
       ["returns", "return"].includes(tag.getTagName())
     );
     // Replace tag with one that contains type info if tag exists
@@ -46,7 +62,23 @@ function generateFunctionDocumentation(functionNode) {
     generateReturnTypeDocumentation(functionNode);
 }
 
-module.exports =  function insertTypes(src, filename) {
+function generateModifierDocumentation(classMemberNode) {
+    const modifiers = classMemberNode.getModifiers() || [];
+    for (let modifier of modifiers) {
+        modifier = modifier?.getText()
+        if (["public", "private", "protected", "readonly", "static"].includes(modifier)) {
+            const jsDoc = getJsDocOrCreate(classMemberNode);
+            jsDoc.addTag({ tagName: modifier })
+        }
+    }
+}
+
+function generateInitializerDocumentation(classPropertyNode) {
+    const jsDoc = getJsDocOrCreate(classPropertyNode);
+    jsDoc.addTag({ tagName: "default", text: classPropertyNode.getStructure().initializer });
+}
+
+module.exports = function insertTypes(src, filename) {
     const project = new Project({
         compilerOptions: {
             target: ScriptTarget.ESNext,
@@ -55,22 +87,23 @@ module.exports =  function insertTypes(src, filename) {
         }
     })
     const sourceFile = project.createSourceFile(path.basename(filename), src)
-    
+
     const classNodes = sourceFile.getClasses()
     for (const classNode of classNodes) {
-        // Generate typings for methods
-        const classMethods = classNode.getInstanceMethods().concat(classNode.getStaticMethods())
-        for (const classMethod of classMethods) {
-            generateFunctionDocumentation(classMethod)
+        const classMembers = classNode.getMembers()
+        for (const classMember of classMembers) {
+            generateModifierDocumentation(classMember)
+            Node.isClassProperty(classMember) && generateInitializerDocumentation(classMember)
+            Node.isMethodDeclaration(classMember) && generateFunctionDocumentation(classMember)
         }
     }
-    
+
     // Generate function parameter type documentation
     const functionNodes = sourceFile.getFunctions()
     for (const functionNode of functionNodes) {
         generateFunctionDocumentation(functionNode)
     }
-    
+
     return project.emitToMemory()?.getFiles()?.[0]?.text || src;
 }
 
