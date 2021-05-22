8ecbb85366f2c7c4d9bc9425c3d4b4c0cb1fcc7a
Futuristick <728608@pdsb.net>
2021-05-21 22:03:03 -0400

    Refactor for better error handling + less internal repetition

--- insert-types.js
+++ insert-types.js
@@ -1,6 +1,51 @@
 const path = require("path");
 const { Project, ScriptTarget, ts } = require("ts-morph");
 
+function generateParameterDocumentation(functionNode) {
+    const params = functionNode.getParameters()
+    for (const param of params) {
+        const paramTypeNode = param.getTypeNode()
+        if (!paramTypeNode) continue;
+        for (const paramTag of (functionNode?.getJsDocs()?.[0]?.getTags() || []).filter(tag =>
+          ["param", "parameter"].includes(tag.getTagName())
+        )) {
+            // Replace tag with one that contains typing info
+            const name = paramTag.compilerNode.name.getText()
+            if (name !== functionNode.getName()) continue;
+            const comment = paramTag.getComment()
+            const parameterType = paramTypeNode.getText()
+            const tagName = paramTag.getTagName()
+            
+            paramTag.replaceWithText(`@${tagName} {${parameterType}} ${name}  ${comment}`)
+        }
+    }
+}
+
+function generateReturnTypeDocumentation(functionNode) {
+    const functionReturnType = functionNode.getReturnType()?.getText()
+    const jsDoc = functionNode.getJsDocs()?.[0]
+    const returnsTag = (jsDoc.getTags() || []).find(tag =>
+      ["returns", "return"].includes(tag.getTagName())
+    );
+    // Replace tag with one that contains type info if tag exists
+    if (returnsTag) {
+        const tagName = returnsTag.getTagName()
+        const comment = returnsTag.getComment()
+        returnsTag.replaceWithText(`@${tagName} {${functionReturnType}}${comment ? ` ${comment}` : ""}`)
+        // Otherwise, create a new one
+    } else {
+        jsDoc.addTag({
+            tagName: "returns",
+            text: `{${functionReturnType}}`
+        })
+    }
+}
+
+function generateFunctionDocumentation(functionNode) {
+    generateParameterDocumentation(functionNode);
+    generateReturnTypeDocumentation(functionNode);
+}
+
 module.exports =  function insertTypes(src, filename) {
     const project = new Project({
         compilerOptions: {
@@ -16,88 +61,16 @@ module.exports =  function insertTypes(src, filename) {
         // Generate typings for methods
         const classMethods = classNode.getInstanceMethods().concat(classNode.getStaticMethods())
         for (const classMethod of classMethods) {
-            // Get the method's actual defined parameters
-            const params = classMethod.getParameters()
-            for (const methodParam of params) {
-                // If there's no type defined for the parameter, don't bother generating a typed tag for it
-                const paramTypeNode = methodParam.getTypeNode()
-                if (!paramTypeNode) continue;
-                
-                // The code that gets all @param or @parameter tags also has to be in the for loop
-                // because the compiler seems to regenerate the node structure whenever a param tag is modified
-                // so assigning it to a variable once doesn't work.
-                // throws error "Attempted to get information from a node that was removed or forgotten."
-                for (const paramTag of classMethod.getJsDocs()[0].getTags().filter(tag =>
-                  ["param", "parameter"].includes(tag.getTagName())
-                )) {
-                    // Find the @param tag that matches the actual parameter
-                    const name = paramTag.compilerNode.name.getText()
-                    if (name !== methodParam.getName()) continue;
-                    const comment = paramTag.getComment()
-                    const parameterType = paramTypeNode.getText()
-                    const tagName = paramTag.getTagName()
-                    // Replace the @param tag with one that includes relevant type information!
-                    paramTag.replaceWithText(`@${tagName} {${parameterType}} ${name}  ${comment}`)
-                }
-            }
-            
-            const methodReturnType = classMethod.getReturnType().getText()
-            const returnsTag = classMethod.getJsDocs()[0].getTags().find(tag =>
-              ["returns", "return"].includes(tag.getTagName())
-            );
-            if (returnsTag) {
-                const tagName = returnsTag.getTagName()
-                const comment = returnsTag.getComment()
-                returnsTag.replaceWithText(`@${tagName} {${methodReturnType}}${comment ? ` ${comment}` : ""}`)
-            } else {
-                classMethod.getJsDocs()[0].addTag({
-                    tagName: "returns",
-                    text: `{${methodReturnType}}`
-                })
-            }
+            generateFunctionDocumentation(classMethod)
         }
     }
     
-    // Generate parameter type documentation
+    // Generate function parameter type documentation
     const functionNodes = sourceFile.getFunctions()
     for (const functionNode of functionNodes) {
-        const params = functionNode.getParameters()
-        for (const param of params) {
-            const paramTypeNode = param.getTypeNode()
-            if (!paramTypeNode) continue;
-            for (const paramTag of functionNode.getJsDocs()[0].getTags().filter(tag =>
-              ["param", "parameter"].includes(tag.getTagName())
-            )) {
-                // Replace tag with one that contains typing info
-                const name = paramTag.compilerNode.name.getText()
-                if (name !== functionNode.getName()) continue;
-                const comment = paramTag.getComment()
-                const parameterType = paramTypeNode.getText()
-                const tagName = paramTag.getTagName()
-
-                paramTag.replaceWithText(`@${tagName} {${parameterType}} ${name}  ${comment}`)
-            }
-        }
-        
-        // Generate return type documentation
-        const functionReturnType = functionNode.getReturnType().getText()
-        const returnsTag = functionNode.getJsDocs()[0].getTags().find(tag =>
-          ["returns", "return"].includes(tag.getTagName())
-        );
-        // Replace tag with one that contains type info if tag exists
-        if (returnsTag) {
-            const tagName = returnsTag.getTagName()
-            const comment = returnsTag.getComment()
-            returnsTag.replaceWithText(`@${tagName} {${functionReturnType}}${comment ? ` ${comment}` : ""}`)
-            // Otherwise, create a new one
-        } else {
-            functionNode.getJsDocs()[0].addTag({
-                tagName: "returns",
-                text: `{${methodReturnType}}`
-            })
-        }
+        generateFunctionDocumentation(functionNode)
     }
     
-    return project.emitToMemory().getFiles()[0].text;
+    return project.emitToMemory()?.getFiles()?.[0]?.text || src;
 }
 
